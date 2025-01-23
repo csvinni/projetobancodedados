0e84e72e7ee83b866ce68732a6cc0bc1c86bcb8c
csvinni <149010256+csvinni@users.noreply.github.com>
2025-01-23 17:23:44 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,95 @@
-from flask import Flask , redirect, url_for, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'db_banco'
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+conexao = MySQL(app)
+
 @app.route('/')
 def index():
-    return 'pagina inicial'
+    return render_template('index.html')
 
 @app.route('/edicao')
 def edicao():
-    return 'edicao'
+    return render_template('edicao.html')
 
-@app.route('/cadastro_doador', methods = (['GET', 'POST']))
-def cadastro_doador():
-    return render_template('cadastro_doador.html')
+@app.route('/doador', methods = (['GET', 'POST']))
+def doador():
+    if request.method == 'POST':
+        cursor = conexao.connection.cursor()
+        nome = request.form.get('nome')
+        telefone = request.form.get('telefone')
+        email = request.form.get('email')
+        INSERT = 'INSERT INTO doadores(nome,email,telefone) VALUES (%s, %s, %s)'
+        
+        try:
+            cursor.execute(INSERT, (nome, email, telefone)) 
+            conexao.connection.commit()  
+        except Exception as e:
+            print(f"An error occurred: {e}")  
+            conexao.connection.rollback()  
+        finally:
+            cursor.close()
 
-@app.route('/cadastro_campanhas')
-def cadastro_campanhas():
-    return 'cadastro_campanhas'
+        return render_template('doador.html')
 
-@app.route('/cadastro_itens_doacoes')
-def cadastro_itens_doacoes():
-    return 'cadastro_itens_doacoes'
+    return render_template('doador.html')
 
+@app.route('/campanhas',  methods = (['GET', 'POST']))
+def campanhas():
+    if request.method == 'POST':
+        cursor = conexao.connection.cursor()
+        titulo = request.form.get('titulo')
+        descricao = request.form.get('descricao')
+        meta_financeira = request.form.get('meta_financeira')
+        meta_itens = request.form.get('meta_itens')
+        data_inicio = request.form.get('data_inicio')
+        data_fim = request.form.get('data_fim')
+        INSERT = 'INSERT INTO campanhas(titulo, descricao, meta_financeira, meta_itens, data_inicio, data_fim) VALUES (%s, %s, %s, %s, %s, %s)'
+        
+        try:
+            cursor.execute(INSERT, (titulo, descricao, meta_financeira, meta_itens, data_inicio, data_fim)) 
+            conexao.connection.commit()  
+        except Exception as e:
+            print(f"An error occurred: {e}")  
+            conexao.connection.rollback()  
+        finally:
+            cursor.close()
+
+        return render_template('campanhas.html')
+    
+    return  render_template('campanhas.html')
+
+@app.route('/itens_doacoes',  methods = (['GET', 'POST']))
+def itens_doacoes():
+    return  render_template('itens_doacoes.html')
+
+@app.route('/listar_campanhas',  methods = (['GET', 'POST']))
+def listar_campanhas():
+
+    cursor = conexao.connection.cursor()  
+    cursor.execute("SELECT * FROM campanhas") 
+    campanhas = cursor.fetchall() 
+    cursor.close()
+
+    return  render_template('listar_campanhas.html', campanhas=campanhas)
+
+
+@app.route('/listar_doadores',  methods = (['GET', 'POST']))
+def listar_doadores():
+    cursor = conexao.connection.cursor()  
+    cursor.execute("SELECT * FROM doadores") 
+    doadores = cursor.fetchall() 
+    cursor.close()
+    return  render_template('listar_doadores.html', doadores=doadores)
+
+@app.route('/relatorios',  methods = (['GET', 'POST']))
+def relatorios():
+    return  render_template('relatorios.html')
