44760422ca5e86de1c5a3c4b8fcf585c29c3d584
Maria Clara <143030148+cllararibeiro@users.noreply.github.com>
2025-02-15 17:16:42 -0300

    Update doador.py

--- controllers/doador.py
+++ controllers/doador.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from sqlalchemy.exc import SQLAlchemyError
 from database.config import Session
 from models.models import Doador
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 doador_bp = Blueprint('doador', __name__, template_folder='templates')
 session = Session()
@@ -14,7 +14,7 @@ def cadastrodoador():
         telefone = request.form.get('telefone')
         email = request.form.get('email')
 
-        novo_doador = Doador(nome=nome, email=email, telefone=telefone)
+        novo_doador = Doador(nome=nome, email=email, telefone=telefone, admin_id=current_user.id)
 
         try:
             session.add(novo_doador)
@@ -31,7 +31,7 @@ def cadastrodoador():
 @login_required
 def listar():
     try:
-        doadores = session.query(Doador).all()  
+        doadores = session.query(Doador).filter_by(admin_id=current_user.id).all()
         return render_template('doador/listar_doadores.html', doadores=doadores)
     except SQLAlchemyError:
         flash('Erro ao buscar doadores. Tente novamente mais tarde.')
